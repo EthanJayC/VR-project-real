a6a73b22fd9fd9de9091c34d79a4d3ec340d38fb
gipsyjaeger <e.j.colbeck@outlook.com>
2022-03-30 18:24:13 +0100

    Office is now destructible >:)

--- VR game/Assets/Scripts/MenuManager.cs
+++ VR game/Assets/Scripts/MenuManager.cs
@@ -6,34 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
     public void ChangeToForest()
     {
-        Debug.Log("DING DONG");
         SceneManager.LoadScene("Forest");
     }
 
     public void ChangeToLiving()
     {
-        Debug.Log("DING DONG");
         SceneManager.LoadScene("LivingRoom");
     }
 
     public void ChangeToOffice()
     {
-        Debug.Log("DING DONG");
         SceneManager.LoadScene("Office");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
