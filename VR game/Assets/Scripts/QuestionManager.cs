4c7d12981a588abcb06d70d0b24181449cd06fe7
Tom Hodgkins <surfytom@gmail.com>
2022-04-01 13:45:27 +0100

    subtitles fixed

--- VR game/Assets/Scripts/QuestionManager.cs
+++ VR game/Assets/Scripts/QuestionManager.cs
@@ -10,8 +10,11 @@ public class QuestionManager : MonoBehaviour
     public AudioClip[] clips;
     public AudioSource Speaker;
     public GameObject ToggleGroup;
+    public Canvas textUI;
     private Toggle[] toggles;
-    private string pathOrigin;
+    public string pathOrigin;
+
+    private string[] subtitiles = { "Hi, I’m Wathanga.\nThank you for taking the time to talk to me today.\nI will be your coach for today’s session.\nAs this is your first session with us we will focus on getting to know you better and will ask you some questions around your feelings and other things that might be impacting you.\nThis is a voluntary session.\nThis means you can choose not to answer any question,there’s no such thing as a wrong answer and we can take a break/end at any time and that’s perfectly OK with me.\n\n(Press Button again to continue)", "How is your mood?\n\nChoose a response that is closest to how you’ve been feeling over the previous week.\n\n(Answer the question on your handheld slate)", "Explain more", "How well do you sleep?\n\nthink about how you’ve been sleeping over the previous week.\n\n(Answer the question on your handheld slate)", "Explain more", "How anxious or on edge do you feel?\n\nChoose an answer that’s closest to how you’ve been feeling over the previous week.\n\n(Answer the question on your handheld slate)", "Explain more", "How stressed do you feel?\n\nChoose an answer that’s closest to how you’ve been feeling over the previous week.\n\n(Answer the question on your handheld slate)", "Explain more", "Have you been worrying about anything?\n\n(Answer the question on your handheld slate)", "Explain more", "Feeling stress is a part of life but it doesn't have to be unmanageable.\nSometimes, it can feel like waves that just keep coming, higher and higher – or like a volcano about to erupt.\n\nLife will always include some stress – that's normal.\n\nStress can affect how you’re feeling or react to things.\nYou might feel worried, nervous or tense.\bYour body might feel different; you might get headaches or feel dizzy or you might get pains in your stomach.\nWe might not be able to make stress go away but we all have the power to manage it, and to take more control." };
 
     public Dialogue[] questions;
     public static QuestionManager instance { get; private set; }
@@ -51,6 +54,7 @@ public class QuestionManager : MonoBehaviour
             if (clipCount <= clips.Length - 1)
             {
                 questions[clipCount].playClip(Speaker, toggles);
+                textUI.GetComponentInChildren<Text>(true).text = subtitiles[clipCount];
                 clipCount++;
                 Debug.Log("Next question playing! clip count: " + clipCount);
             }
